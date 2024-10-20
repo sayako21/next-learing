6b65faf3f0414d4fff2ee459f6b459db0e66716c
sayako21 <>
2024-10-20 09:41:30 +0900

    [追加]nextチュートリアル

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import TaskTable from "@/components/taskTable";
+
+export default async function Index() {
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center pt-24">
+      <TaskTable />
+    </div>
+  )
+}
